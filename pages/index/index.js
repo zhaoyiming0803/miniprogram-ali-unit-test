98f302b3035f8ea403ed50beed1a792f9ef4986f
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-08-03 16:01:24 +0800

    test

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,7 +13,7 @@ enhancePage({
     const res1 = await this.getRes1()
     this.setData({
       // a: res1.a,
-      b: res1.b
+      // b: res1.b
     })
     const res2 = await this.getRes2()
     this.setData({
